3c52d755fbe1d9c35d793d2a9002a2b19e3993a0
zhang shao qi <2147376322@qq.com>
2026-04-15 20:04:02 +0800

    初始化项目，添加主程序框架，完成学生信息管理系统v1.0 - 实现添加学生功能

--- student-management.py
+++ student-management.py
@@ -1,4 +1,37 @@
 class Student:
-    def __init__(self, name, age,):
-        self.first_name = first_name
-        self.last_name = last_name
+    def __init__(self,name,gender,age,address):
+        self.name = name
+        self.gander = gender
+        self.age = age
+        self.address = address
+def add_student():
+    f = open("students.txt", "a", encoding="utf-8")
+    while True:
+        print("请录入学生信息")
+        name = input("请输入学生姓名：")
+        gender = input("请输入学生性别")
+        age = input("请输入学生年龄：")
+        address = input("请输入学生家庭地址：")
+        student = Student(name,gender,age,address)
+        f.write(f"学生姓名：{student.name},性别：{student.gander},年龄：{student.age},家庭住址:{student.address}\n")
+        print("当前学生信息录入完毕，是否继续录入？继续录入请输入1，退出录入请输入0")
+        choose = input("请确认是否继续录入：")
+        if choose == "1":
+            continue
+        else:
+            break
+        f.close()
+
+def main():
+    while True:
+        print("欢迎进入学生信息管理系统，请选择操作，输入1增加学生信息,输入2删除删除学生信息，输入3修改学生信息，输入4查询学生信息")
+        num = int(input("请输入数字："))
+        if num == 1:
+            add_student()
+        elif num == 2:
+            print("删除功能待实现")
+
+
+
+if __name__ == '__main__':
+    main()
