db18c7702dc17c9d476cca57ab1b38bc810a7e85
zhang shao qi <2147376322@qq.com>
2026-04-16 21:36:20 +0800

    实现信息修改功能

--- student-management.py
+++ student-management.py
@@ -5,25 +5,28 @@ class Student:
         self.age = age
         self.address = address
 
+#定义学生id并设置id自增
 def get_next_id():
     max_id = 0
+#捕获异常，可能电脑上没这个文件
     try:
-       f = open("students.txt", "r", encoding="utf-8")
-       student_read = f.readlines()
-       f.close()
-       for student_data in student_read:
-           student_id = int(student_data.split(',')[0])
-           if student_data.strip() == "":
-               continue
-           if student_id > max_id:
-                max_id = student_id
-       return max_id + 1
+        with open("students.txt", "r", encoding="utf-8") as f:
+            student_read = f.readlines()  # 读取文件内容
+            for student_data in student_read:  # 遍历，将数据转换成列表封装在student_data中
+                if not student_data.strip():
+                    continue
+                student_id = int(student_data.split(',')[0])  # 对列表进行切分，并通过索引获取id，转换成整数类型
+                if student_id > max_id:  # 实现增加一条学生信息，id自增
+                    max_id = student_id
+        return max_id + 1
     except FileNotFoundError:
         return 1
 
 
+
+#定义函数，用来增加学生信息
 def add_student():
-    next_id = get_next_id()
+    next_id = get_next_id()          #调用get_next_id函数，自动为录入的学生信息设置id
     with open("students.txt", "a", encoding="utf-8") as f:
         while True:
             print("请录入学生信息")
@@ -32,7 +35,9 @@ def add_student():
             age = input("请输入学生年龄：")
             address = input("请输入学生家庭地址：")
             student = Student(name, gender, age, address)
-            f.write(f"{next_id}{student.name},{student.gander},{student.age},{student.address}\n")
+            f.write(f"{next_id},{student.name},{student.gander},{student.age},{student.address}\n")
+            print(f"学生{name}添加成功，ID={next_id}")
+            next_id += 1
             print("当前学生信息录入完毕，是否继续录入？输入1继续录入，否则退出。")
             choose = input("请确认是否继续录入：")
             if choose == "1":
@@ -43,11 +48,10 @@ def add_student():
 
 
 
-
 def del_student():
     sid = int(input("请输入要删除的学生id："))
     try:
-        with open("students.txt", "r", encoding="utf-8") as f:
+        with open("students.txt", "r", encoding="utf-8") as f:   #捕获找不到文件的异常
             student_read = f.readlines()
     except FileNotFoundError:
         print("文件不存在")
@@ -57,10 +61,10 @@ def del_student():
         return
     new_student_read = []
     for student_data in student_read:
-        if not student_data.strip():
+        if not student_data.strip():       #设置跳过空行
             continue
         try:
-            list_id_data = int((student_data.split(',')[0]))
+            list_id_data = int((student_data.split(',')[0]))      #捕获输入不是整数时的异常
         except:
             continue
         if list_id_data != sid:
@@ -75,18 +79,73 @@ def del_student():
 
 
 
+def find_student_by_id(sid):
+    try:
+        with open("students.txt", "r", encoding="utf-8") as f:
+            student_read = f.readlines()
+            for student_data in student_read:
+                list_id_data = int((student_data.split(',')[0]))
+                if list_id_data != sid:
+                    continue
+                else:
+                    return student_data
+    except FileNotFoundError:
+        print("文件不存在")
+        return None
+    return None
+
+
+
+def updata_student():
+    sid = int(input("请输入要修改的学生id："))
+    result = find_student_by_id(sid)
+    if result is None:
+        print("未找到该学生")
+        return
+    with open("students.txt", "r", encoding="utf-8") as f:
+        student_read = f.readlines()
+        idx = -1
+        for i,student_data in enumerate(student_read):
+            if student_data.strip() == result.strip():
+                idx = i
+                break
+        if idx == -1:
+            print("未找到索引")
+            return
+        update = student_read[idx].strip().split(',')
+        print(f"当前信息：姓名：{update[1]},性别：{update[2]},年龄：{update[3]},地址：{update[4]}")
+        choose = input("请输入要修改的字段：1姓名 2性别 3年龄 4地址")
+        if choose == "1":
+            update[1] = input("新姓名：")
+        elif choose == "2":
+            update[2] = input("新性别：")
+        elif choose == "3":
+            update[3] = input("新年龄：")
+        elif choose == "4":
+            update[4] = input("新地址：")
+        else:
+            print("无效选择")
+            return
+    new_student_read = ','.join(update) + '\n'
+    student_read[idx] = new_student_read
+    with open("students.txt", "w", encoding="utf-8") as f:
+        f.writelines(student_read)
+    print("修改成功")
+
+
+
 
 
 def main():
     while True:
-        print("欢迎进入学生信息管理系统，请选择操作，输入1增加学生信息,输入2删除删除学生信息，输入3修改学生信息，输入4查询学生信息")
+        print("欢迎进入学生信息管理系统，请选择操作，输入1增加学生信息,输入2删除学生信息，输入3修改学生信息，输入4查询学生信息")
         num = int(input("请输入数字："))
         if num == 1:
             add_student()
         elif num == 2:
             del_student()
         elif num == 3:
-            print("修改功能待实现")
+            updata_student()
         elif num == 4:
             print("查询功能待实现")
         else:
